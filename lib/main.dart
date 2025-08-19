3501af6273ca587b5e5d193948d1bf06ef3a4b62
Anirudh <anirudh157290@gmail.com>
2025-08-19 12:33:55 +0530

    Initial commit - Flutter ToDo App

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do/pages/home_page.dart';
+
+void main() async {
+  // init the hive
+  await Hive.initFlutter();
+
+  // open the box
+  await Hive.openBox('mybox');
+
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.yellow),
+        useMaterial3: false,
+      ),
+
+    );
+    
+  }
+}
---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,56 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:to_do/util/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  const DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  final TextEditingController controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[200],
+      content: SizedBox(
+        height: 120,
+        child: Column(
+          children: [
+          //get user input 
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: "Add a new task",
+              
+            ),
+          ),
+
+          //buttons -> save + cancel
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              // save button 
+              MyButton(text: "Save", onPressed: onSave),
+
+              const SizedBox(width: 8),
+
+              // cancel button
+              MyButton(text: "Cancel", onPressed: onCancel),
+            ],
+          ),
+       ]),
+     ),
+    );
+
+  }
+}
